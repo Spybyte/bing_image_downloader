fbb3e5d9fce1933e7809f33eba87e4bd8a562283
bjoern <bjoern@bubbat.de>
2020-12-16 11:42:37 +0100

    minor modification for logging and multithreading

--- bing_image_downloader/bing.py
+++ bing_image_downloader/bing.py
@@ -35,7 +35,7 @@ class Bing:
         request = urllib.request.Request(link, None, self.headers)
         image = urllib.request.urlopen(request, timeout=self.timeout).read()
         if not imghdr.what(None, image):
-            print('[Error]Invalid image, not saving {}\n'.format(link))
+            print(f'[Error] {self.query}: Invalid image, not saving {link}\n')
             raise
         with open(file_path, 'wb') as f:
             f.write(image)
@@ -52,18 +52,18 @@ class Bing:
                 file_type = "jpg"
 
             # Download the image
-            print("[%] Downloading Image #{} from {}".format(self.download_count, link))
+            print(f"[%] {self.query}: Downloading Image #{self.download_count} from {link}")
 
             self.save_image(link, "{}/{}/{}/".format(os.getcwd(), self.output_dir, self.query) + "Image_{}.{}".format(
                 str(self.download_count), file_type))
-            print("[%] File Downloaded !\n")
+            print(f"[%] {self.query}: File Downloaded !\n")
         except Exception as e:
             self.download_count -= 1
-            print("[!] Issue getting: {}\n[!] Error:: {}".format(link, e))
+            print(f"[!] {self.query}: Issue getting: {link}\n[!] {self.query}: Error:: {e}")
 
     def run(self):
         while self.download_count < self.limit:
-            print('\n\n[!!]Indexing page: {}\n'.format(self.page_counter + 1))
+            print(f'\n\n[!] {self.query}: Indexing page: {self.page_counter+1}\n')
             # Parse the page source and download pics
             request_url = 'https://www.bing.com/images/async?q=' + urllib.parse.quote_plus(self.query) \
                 + '&first=' + str(self.page_counter) + '&count=' + str(self.limit) \
@@ -73,14 +73,14 @@ class Bing:
             html = response.read().decode('utf8')
             links = re.findall('murl&quot;:&quot;(.*?)&quot;', html)
 
-            print("[%] Indexed {} Images on Page {}.".format(len(links), self.page_counter + 1))
+            print(f"[%] {self.query}: Indexed {len(links)} Images on Page {self.page_counter+1}.")
             print("\n===============================================\n")
 
             for link in links:
                 if self.download_count < self.limit:
                     self.download_image(link)
                 else:
-                    print("\n\n[%] Done. Downloaded {} images.".format(self.download_count))
+                    print(f"\n\n[%] {self.query}: Done. Downloaded {self.download_count} images.")
                     print("\n===============================================\n")
                     break
 
--- build/lib/bing_image_downloader/bing.py
+++ build/lib/bing_image_downloader/bing.py
@@ -1,10 +1,10 @@
 from pathlib import Path
+from os.path import basename
 import os
 import sys
 import urllib.request
 import urllib
 import imghdr
-import posixpath
 import re
 
 '''
@@ -14,7 +14,7 @@ Author: Guru Prasad (g.gaurav541@gmail.com)
 
 
 class Bing:
-    def __init__(self, query, limit, output_dir, adult, timeout, filters=''):
+    def __init__(self, query, limit, output_dir, adult, timeout, filters='', headers={}):
         self.download_count = 0
         self.query = query
         self.output_dir = output_dir
@@ -27,13 +27,15 @@ class Bing:
         self.timeout = timeout
 
         self.headers = {'User-Agent': 'Mozilla/5.0 (X11; Fedora; Linux x86_64; rv:60.0) Gecko/20100101 Firefox/60.0'}
+        self.headers.update(headers)
+
         self.page_counter = 0
 
     def save_image(self, link, file_path):
         request = urllib.request.Request(link, None, self.headers)
         image = urllib.request.urlopen(request, timeout=self.timeout).read()
         if not imghdr.what(None, image):
-            print('[Error]Invalid image, not saving {}\n'.format(link))
+            print(f'[Error] {self.query}: Invalid image, not saving {link}\n')
             raise
         with open(file_path, 'wb') as f:
             f.write(image)
@@ -44,41 +46,41 @@ class Bing:
         # Get the image link
         try:
             path = urllib.parse.urlsplit(link).path
-            filename = posixpath.basename(path).split('?')[0]
+            filename = basename(path).split('?')[0]
             file_type = filename.split(".")[-1]
             if file_type.lower() not in ["jpe", "jpeg", "jfif", "exif", "tiff", "gif", "bmp", "png", "webp", "jpg"]:
                 file_type = "jpg"
 
             # Download the image
-            print("[%] Downloading Image #{} from {}".format(self.download_count, link))
+            print(f"[%] {self.query}: Downloading Image #{self.download_count} from {link}")
 
             self.save_image(link, "{}/{}/{}/".format(os.getcwd(), self.output_dir, self.query) + "Image_{}.{}".format(
                 str(self.download_count), file_type))
-            print("[%] File Downloaded !\n")
+            print(f"[%] {self.query}: File Downloaded !\n")
         except Exception as e:
             self.download_count -= 1
-            print("[!] Issue getting: {}\n[!] Error:: {}".format(link, e))
+            print(f"[!] {self.query}: Issue getting: {link}\n[!] {self.query}: Error:: {e}")
 
     def run(self):
         while self.download_count < self.limit:
-            print('\n\n[!!]Indexing page: {}\n'.format(self.page_counter + 1))
+            print(f'\n\n[!] {self.query}: Indexing page: {self.page_counter+1}\n')
             # Parse the page source and download pics
             request_url = 'https://www.bing.com/images/async?q=' + urllib.parse.quote_plus(self.query) \
-                          + '&first=' + str(self.page_counter) + '&count=' + str(self.limit) \
-                          + '&adlt=' + self.adult + '&qft=' + self.filters
+                + '&first=' + str(self.page_counter) + '&count=' + str(self.limit) \
+                + '&adlt=' + self.adult + '&qft=' + self.filters
             request = urllib.request.Request(request_url, None, headers=self.headers)
             response = urllib.request.urlopen(request)
             html = response.read().decode('utf8')
             links = re.findall('murl&quot;:&quot;(.*?)&quot;', html)
 
-            print("[%] Indexed {} Images on Page {}.".format(len(links), self.page_counter + 1))
+            print(f"[%] {self.query}: Indexed {len(links)} Images on Page {self.page_counter+1}.")
             print("\n===============================================\n")
 
             for link in links:
                 if self.download_count < self.limit:
                     self.download_image(link)
                 else:
-                    print("\n\n[%] Done. Downloaded {} images.".format(self.download_count))
+                    print(f"\n\n[%] {self.query}: Done. Downloaded {self.download_count} images.")
                     print("\n===============================================\n")
                     break
 
