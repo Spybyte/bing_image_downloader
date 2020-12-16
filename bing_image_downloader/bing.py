7dc8ee9626e0a489a39942d2ded0f1d577e3fe2e
Spybyte <spybyte@online.de>
2020-12-16 10:38:36 +0100

    added initial langauges support

--- bing_image_downloader/bing.py
+++ bing_image_downloader/bing.py
@@ -1,10 +1,10 @@
 from pathlib import Path
+from os.path import basename
 import os
 import sys
 import urllib.request
 import urllib
 import imghdr
-import posixpath
 import re
 
 '''
@@ -14,7 +14,7 @@ Author: Guru Prasad (g.gaurav541@gmail.com)
 
 
 class Bing:
-    def __init__(self, query, limit, output_dir, adult, timeout, filters=''):
+    def __init__(self, query, limit, output_dir, adult, timeout, filters='', headers={}):
         self.download_count = 0
         self.query = query
         self.output_dir = output_dir
@@ -27,6 +27,8 @@ class Bing:
         self.timeout = timeout
 
         self.headers = {'User-Agent': 'Mozilla/5.0 (X11; Fedora; Linux x86_64; rv:60.0) Gecko/20100101 Firefox/60.0'}
+        self.headers.update(headers)
+
         self.page_counter = 0
 
     def save_image(self, link, file_path):
@@ -44,7 +46,7 @@ class Bing:
         # Get the image link
         try:
             path = urllib.parse.urlsplit(link).path
-            filename = posixpath.basename(path).split('?')[0]
+            filename = basename(path).split('?')[0]
             file_type = filename.split(".")[-1]
             if file_type.lower() not in ["jpe", "jpeg", "jfif", "exif", "tiff", "gif", "bmp", "png", "webp", "jpg"]:
                 file_type = "jpg"
@@ -64,8 +66,8 @@ class Bing:
             print('\n\n[!!]Indexing page: {}\n'.format(self.page_counter + 1))
             # Parse the page source and download pics
             request_url = 'https://www.bing.com/images/async?q=' + urllib.parse.quote_plus(self.query) \
-                          + '&first=' + str(self.page_counter) + '&count=' + str(self.limit) \
-                          + '&adlt=' + self.adult + '&qft=' + self.filters
+                + '&first=' + str(self.page_counter) + '&count=' + str(self.limit) \
+                + '&adlt=' + self.adult + '&qft=' + self.filters
             request = urllib.request.Request(request_url, None, headers=self.headers)
             response = urllib.request.urlopen(request)
             html = response.read().decode('utf8')
